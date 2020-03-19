2f7a1287e8ee015cf6e512667497e4f3c7400ed8
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2020-03-19 15:28:12 -0300

    database integration and relations with teachers and students

---
+++ src/app/controllers/students.js
@@ -0,0 +1,72 @@
+const { date, grade, age } = require("../../lib/utils");
+
+const Student = require("../models/student");
+
+module.exports = {
+  index(req, res) {
+    Student.all(function(students) {
+      let studentsArray = [];
+
+      students.forEach(function (student) {
+        student = {
+          ...student,
+          grade: grade(student.grade),
+        }
+        return studentsArray.push(student)
+      })
+      return res.render("students/index", { students: studentsArray });
+    });
+  },
+  create(req, res) {
+    return res.render("students/create");
+  },
+  post(req, res) {
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == "")
+        return res.send("Por favor, preencha todos os campos")
+    }
+
+    Student.create(req.body, function(student) {
+      return res.redirect(`/students/${student.id}`)
+    })
+  },
+  show(req, res) {
+    Student.find(req.params.id, function(student) {
+      if (!student) res.send("Aluno não encontrado!");
+
+      student.birth_date = age(student.birth_date);
+      student.grade = grade(student.grade);
+
+      return res.render("students/show", { student })
+    });
+  },
+  edit(req, res) {
+    Student.find(req.params.id, function(student) {
+      if (!student) return res.send("Aluno não encontrado!");
+      
+      student.birth_date = date(student.birth_date).iso;
+
+      return res.render("students/edit", {student});
+    });
+  },
+  put(req, res) {
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == "")
+        return res.send("Por favor, preencha todos os campos")
+    }
+
+    Student.update(req.body, function() {
+      return res.redirect(`/students/${req.body.id}`)
+    });
+  },
+  delete(req, res) {
+    Student.delete(req.body.id, function () {
+      return res.redirect(`/students`);
+    });
+  },
+}
+  
--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ server.set("view engine", "njk");
 
 nunjucks.configure ("src/app/views", {
   express: server,
+  autoescape: false,
   noCache: true
 });
 
